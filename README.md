47ccf7d3c89adac773b92daec441f81b2cbf0880
vanya andreychuk <vandreychyk@gmail.com>
2017-09-05 21:37:32 +0300

    - update README - remove lodash from package.json

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # postgresql-to-rabbitmq-notify
+
+## How to use
+
+    postgres=# SELECT pg_notify('messenger', json_message);
+     pg_notify
+     -----------
+    
+     (1 row)
+
+## Message data structure
+    
+    RabbitMq Type: topic
+      
+    Message data:
+    {
+        "exchange" - Exchange name
+        "data" - Document object
+    }     
+
+## Environment Variables
+
+    - POSTGRES_PASSWORD - postresql db password
+    - POSTGRES_USER - postgresql db user
+    - POSTGRES_DATABASE - postgresql db name
+    - POSTGRES_HOST - postgresql host
+    - POSTGRES_PORT- postgresql port
+    - RABBIT_HOST - rabbitmq host
+    - RABBIT_PORT - rabbitmq post
+    - RABBIT_USER - rabbitmq user
+    - RABBIT_PASSWORD - rabbitmq password
+ 
--- app/listener.js
+++ app/listener.js
@@ -2,18 +2,17 @@
 
 const amqp = require('amqplib/callback_api');
 const PS = require('pg-pubsub');
-const _ = require('lodash');
 
 const rabbitConnectionString = 'amqp://' + process.env.RABBIT_USER + ':'
   + process.env.RABBIT_PASSWORD + '@' + process.env.RABBIT_HOST + ':'
-  + process.env.RABBIT_PORT || 'amqp://guest:guest@rabbitmq';
+  + process.env.RABBIT_PORT;
 
 const cn = {
-    host: process.env.POSTGRES_HOST || "postgresql", // server name or IP address;
-    port: process.env.POSTGRES_PORT || 5432,
-    database: process.env.POSTGRES_DATABASE || "test",
-    user: process.env.POSTGRES_USER || "postgres",
-    password: process.env.POSTGRES_PASSWORD || "DGH5k39xchgh3Po",
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DATABASE,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
     getConnectionString: function () {
         return 'postgres://' + this.user + ':' + this.password + '@' + this.host + '/' + this.database;
     }
@@ -21,15 +20,13 @@ const cn = {
 
 const ps = new PS(cn.getConnectionString());
 
-var exchangeList = [];
-
 ps.addChannel('messenger', function(payload){
     amqp.connect(rabbitConnectionString, function(err, conn) {
         conn.createChannel(function(err, ch) {
             ch.assertExchange(payload.exchange, 'topic', {durable: false});
             ch.publish(payload.exchange, "", new Buffer(JSON.stringify(payload.data)));
             console.log(" [x] Exchange:  ", payload.exchange);
-            console.log(" [x] Messenger: ", payload.data);
+            console.log(" [x] Data:      ", payload.data);
         });
     });
 });
